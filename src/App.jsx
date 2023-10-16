c3dd8415ad7e6de061a2937475c220d8da02bccb
Marc Chacon <marcchacon1@outlook.com>
2023-10-16 08:55:46 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
+
+export default function App() {
+
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
+
