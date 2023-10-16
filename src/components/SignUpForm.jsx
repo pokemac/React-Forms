01007f0428f785e86b3ee989948092e6bbc3c535
Marc Chacon <marcchacon1@outlook.com>
2023-10-16 09:12:27 -0500

    added useState to SignUp file

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,6 @@
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
+export default function SignUpForm() {
+    return <h2>Sign Up!</h2>;
+}
