0cbd5e19b8791ee8810becef59783dbbde940a57
Marc Chacon <marcchacon1@outlook.com>
2023-10-16 09:38:04 -0500

    finished authenticate file

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,3 +1,37 @@
-export default function Authenticate() {
-    return <h2>Authenticate!</h2>
-}
+import { useState } from "react";
+
+export default function Authenticate({token}){
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+
+    async function handleClick(e) {
+       e.preventDefault();
+
+        try{
+          const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
+            { 
+              method: "GET", 
+              headers: { 
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}` 
+              }
+            })
+          const result = await response.json();
+          setSuccessMessage(result.message);
+        } catch (error) {
+          setError(error.message);
+        }
+    }
+
+    return ( 
+        <div>
+        <h2>Authenticate!</h2>
+        
+        {  }
+        {successMessage && <p>{successMessage}</p>}
+        {error && <p>{error}</p>}
+
+        <button onClick={handleClick}>Authenticate </button> 
+        </div>
+    )
+  }
